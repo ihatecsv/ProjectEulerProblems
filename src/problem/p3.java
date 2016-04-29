c8ec9ff28bdc804da4b93263110403e47858fa37
Drake <drakeluce@gmail.com>
2016-04-29 12:54:54 -0300

    Trying to figure out BigInteger math

---
+++ src/problem/p3.java
@@ -0,0 +1,25 @@
+package problem;
+
+import java.math.BigInteger;
+
+public class p3 {
+    //INCOMPLETE
+    /*
+    public static void main(String[] args){
+        BigInteger bd = new BigInteger("600851475143");
+        BigInteger largestFactor = getLargestFactor(bd);
+        System.out.println(largestFactor);
+    }
+
+    public static BigInteger getLargestFactor(BigInteger num){
+        BigInteger i = new BigInteger("0");
+        BigInteger sqrtNum = num.
+        for(; i.compareTo()Math.sqrt(num) == -1; i = i.add(new BigInteger("1"))){
+            if(num.mod(i).equals(new BigInteger("0")) && !i.equals(num)){
+                return i;
+            }
+        }
+        return new BigInteger("0");
+    }
+    */
+}
